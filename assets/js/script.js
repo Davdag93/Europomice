a9b5d3459f8dc635450899b8e1277ba9100c3c24
Davide D'Agostino <davide-d.agostino@outlook.com>
2023-03-20 14:33:39 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const button1 = document.getElementById("change-text-btn-1");
+const button2 = document.getElementById("change-text-btn-2");
+const button3 = document.getElementById("change-text-btn-3");
+const text = document.getElementById("text");
+
+button1.addEventListener("click", () => {
+  text.textContent = "FLOROVIVAISMO";
+});
+
+button2.addEventListener("click", () => {
+  text.textContent = "AREE VERDI";
+});
+
+button3.addEventListener("click", () => {
+  text.textContent = "EDILIZIA";
+});
+
+
+
+const toggleBtn = document.querySelector('.toggleBtn');
+const myDivs = document.querySelectorAll('.myDiv');
+
+toggleBtn.addEventListener('click', function() {
+  myDivs.forEach(function(myDiv) {
+    myDiv.classList.toggle('myClass');
+  });
+}); 
